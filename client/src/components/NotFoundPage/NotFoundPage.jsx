5fb735d88bbde29271952d9ac2369836eb5b7fb6
Rasulio05 <mrn_0593@mail.ru>
2025-01-31 11:58:44 +0300

    Кончное

--- client/src/components/NotFoundPage/NotFoundPage.jsx
+++ client/src/components/NotFoundPage/NotFoundPage.jsx
@@ -1,21 +1,3 @@
-// import React from 'react';
-// import { Button } from 'semantic-ui-react';
-// import styles from './NotFoundPage.module.css';
-
-// export default function NotFoundPage() {
-//   const handlerGetBack = () => {
-//     window.history.back();
-//   };
-//   return (
-//     <div className={styles.container}>
-//       <div>
-//         <Button onClick={() => handlerGetBack()}>{'<- Вернуться назад'}</Button>
-//         <img src="MV5BOGVmZTZmYTctZDU1Ni00ZTA1LWI0NTktYjQ4NWViNWYwOWFiXkEyXkFqcGdeQXVyNjcwNDUyODM@._V1_-fotor-bg-remover-2025013110510.png" className='img'></img>
-//       </div>
-//       <h1>Я Раскажу твоей маме что ты балуешься</h1>
-//     </div>
-//   );
-// }
 import React from 'react';
 import { Button } from 'semantic-ui-react';
 import styles from './NotFoundPage.module.css';
@@ -28,7 +10,9 @@ export default function NotFoundPage() {
   return (
     <div className={styles.container}>
       <div className={styles.buttonWrapper}>
-        <Button onClick={handlerGetBack}>{'<- Вернуться назад'}</Button>
+        <Button primary onClick={handlerGetBack}>
+          {'<- Вернуться назад'}
+        </Button>
       </div>
       <img
         src="MV5BOGVmZTZmYTctZDU1Ni00ZTA1LWI0NTktYjQ4NWViNWYwOWFiXkEyXkFqcGdeQXVyNjcwNDUyODM@._V1_-fotor-bg-remover-2025013110510.png"
